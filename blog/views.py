7287257d7bfe641ac8d00cfd5cb36a037f3eaf09
dspring86 <dspring@equityeng.com>
2016-01-27 07:46:44 -0500

    Changed

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def boot_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/index.html')
+# def boot_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/index.html')
